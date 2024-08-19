b35e6ce2c34544ea1834a24d114e1af6c4a22680
virajbk <viraj@lazot.com>
2024-08-19 08:39:44 +0530

    Testing if npm run deploy still works

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+// import Home from "./pages/Home/Home.js";
+// import Projects from "./pages/Projects/Projects.js";
+// import Blog from "./pages/Blog/Blog.js";
+// import Opensource from "./pages/Opensource/Opensource.js";
+// import Tutoring from "./pages/Dsapage/Dsapage.js";
+// import Dsapage from "./pages/Dsapage/Dsapage.js";
+
+import { useDispatch } from "react-redux";
+import { Routes, Route } from "react-router";
+
 function App() {
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi friends.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Newpage</p>
       </header>
     </div>
   );
---
+++ src/pages/Blog/Blog.js
@@ -0,0 +1,5 @@
+import React from "react";
+const Blog = () => {
+    return <div>Blog</div>
+};
+export default Blog;
